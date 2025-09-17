7109b7140d19f71f523b1164f4c8c1be4820624d
Daniel Pinto <daniel2190558@correo.uis.edu.co>
2025-09-17 16:15:38 -0500

    style(layout): add overflow-hidden to body element
    Prevents unwanted scrolling when modal dialogs are open

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           rel="stylesheet"
         ></link>
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased overflow-hidden">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
